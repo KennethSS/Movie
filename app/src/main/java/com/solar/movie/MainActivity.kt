ddea1ef9c22b254ffe27556aeaf4f6ce6e7d94f2
kenneth <wlsrhkd4023@gmail.com>
2021-01-14 16:41:13 +0900

    add empty case to api key

--- app/src/main/java/com/solar/movie/MainActivity.kt
+++ app/src/main/java/com/solar/movie/MainActivity.kt
@@ -2,24 +2,24 @@ package com.solar.movie
 
 import android.os.Bundle
 import android.view.Menu
+import android.widget.Toast
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import com.solar.library.binding.activity.BindingActivity
 import com.solar.movie.databinding.ActivityMainBinding
+import com.solar.movie.remote.MOVIE_KEY
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : BindingActivity() {
-
     private val binding: ActivityMainBinding by binding(R.layout.activity_main)
-
-    private lateinit var navController: NavController
-
-    var bottomBarMenu: Menu? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
+        if (MOVIE_KEY.isEmpty()) {
+            Toast.makeText(this, R.string.api_key_empty, Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
